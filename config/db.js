206d569a602939705802956b16809b2a019a58eb
HAKHEO <pigpannyaung@gmail.com>
2026-06-19 11:23:17 +0630

    New

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
 import "dotenv/config";
 import mysql from "mysql2/promise";
 
+console.log("ENV CHECK =>", {
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASS: process.env.PASS,
+  DB: process.env.DB,
+});
+
 const db = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
