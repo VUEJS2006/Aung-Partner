758bc0a4b438d2322da182e192ecba0cf1cfd84e
HAKHEO <pigpannyaung@gmail.com>
2026-06-22 19:47:04 +0630

    Bagan

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ import mysql from "mysql2/promise";
 
 console.log("ENV CHECK =>", {
   HOST: process.env.HOST,
-  USER: process.env.USER,
+  USER: process.env.DB_USER,
   PASS: process.env.PASS,
   DB: process.env.DB,
 });
 
 const db = mysql.createPool({
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.DB_USER,
   password: process.env.PASS,
   database: process.env.DB,
 
