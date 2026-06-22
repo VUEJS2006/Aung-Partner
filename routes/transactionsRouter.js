732ee0b388dfebdb247769ce21d1f975a881e06e
HAKHEO <pigpannyaung@gmail.com>
2026-06-22 22:33:55 +0630

    Last

--- routes/transactionsRouter.js
+++ routes/transactionsRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getLastAmountByUser,LastAmountInsert,DividendListByUser,TransactionTotal,BuyListByUser,BuyStatus, DividendStatus, TransactionsCreate, TransactionsList, userTransactionsList, createLastAmount, lastAmountList, userLastAmount, userLastAmountUpdate } from "../controllers/transactionsController.js"
+import { getLastAmountByUser,LastAmountInsert,DividendListByUser,TransactionTotal,BuyListByUser,BuyStatus, DividendStatus, TransactionsCreate, TransactionsList, userTransactionsList } from "../controllers/transactionsController.js"
 import { validateRegister, isAdmin, isAuth } from "../middleware/authMiddleware.js";
 import { adminPasscode } from "../middleware/passcodeMiddleware.js";
 const router = express.Router();
@@ -8,11 +8,11 @@ const router = express.Router();
 
 router.get("/list/share/transactions", isAuth, isAdmin, TransactionsList);
 router.post("/create/share/transactions", isAuth, isAdmin, adminPasscode, TransactionsCreate);
-router.post("/create/last-amount", isAuth, isAdmin, createLastAmount);
-router.get("/list/last-amount", isAuth, isAdmin, lastAmountList);
+// router.post("/create/last-amount", isAuth, isAdmin, createLastAmount);
+// router.get("/list/last-amount", isAuth, isAdmin, lastAmountList);
 router.get("/v1/list/share/transactions", isAuth, userTransactionsList);
-router.get("/v1/list/last-amount", isAuth, userLastAmount);
-router.put("/v1/update/last-amount/:id", isAuth, userLastAmountUpdate);
+// router.get("/v1/list/last-amount", isAuth, userLastAmount);
+// router.put("/v1/update/last-amount/:id", isAuth, userLastAmountUpdate);
 router.get("/v1/buy/status", isAuth, BuyStatus);
 router.get("/v1/dividend/status", isAuth, DividendStatus);
 router.get("/list/buy/status/:id",isAuth,isAdmin,BuyListByUser)
